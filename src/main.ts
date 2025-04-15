7c163ba46d710fb3d27b673b852d0ee7b43ad404
Martin <martin.moguillansky@gmail.com>
2025-04-15 19:27:17 -0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://libelula-space.netlify.app/'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
