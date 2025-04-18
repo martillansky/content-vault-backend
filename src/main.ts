f0e56a2ebe8b808d52b3561f4bc6405427466820
Martin <martin.moguillansky@gmail.com>
2025-04-18 19:22:20 -0300

    fix: updated CORS config

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://libelula-space.netlify.app/'],
+    origin: ['http://localhost:3000', 'https://libelula-space.netlify.app'],
     methods: ['GET', 'POST'],
     credentials: true,
   });
