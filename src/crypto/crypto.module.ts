eed1c06acf6134e3822905b41852d4e3d6bf486c
Martin <martin.moguillansky@gmail.com>
2025-04-18 16:05:50 -0300

    fix: supabase import on crypto.module

--- src/crypto/crypto.module.ts
+++ src/crypto/crypto.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { SupabaseModule } from 'src/supabase/supabase.module';
 import { CryptoService } from './crypto.service';
 
 @Module({
+  imports: [SupabaseModule],
   providers: [CryptoService],
   exports: [CryptoService],
 })
